eb47efddb96377eb265da22814688c817dce3727
saeed rastegar <saeed.rastegarrazaghi@tu-clausthal.de>
2023-07-24 15:35:13 +0200

    props added

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,10 +1,17 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+    const expenses = [
+        { title: 'Car Insurance', amount: 294.67, date: new Date(2021, 2, 28)},
+        { title: 'House Insurance', amount: 694.67, date: new Date(2022, 2, 28)},
+        { title: 'Office Insurance', amount: 1294.67, date: new Date(2023, 2, 28)},
+    ]
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem />
+        <ExpenseItem title = {expenses[0].title} amount = {expenses[0].amount} date = {expenses[0].date}></ExpenseItem>
+        <ExpenseItem title = {expenses[0].title} amount = {expenses[0].amount} date = {expenses[0].date}></ExpenseItem>
+        <ExpenseItem title = {expenses[0].title} amount = {expenses[0].amount} date = {expenses[0].date}></ExpenseItem>
     </div>
   );
 }
